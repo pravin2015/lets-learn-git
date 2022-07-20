6e664e07313ed5685897f3dbdf3993996adf2372
Pravinkumar Chavan <pravin.chavan999@gmail.com>
2022-07-20 21:59:45 +0530

    adding diff.java and modified sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ class sum{
     public static void main(string[] args){
         int a=5;
         int b=10;
-        System.out.println("Summation =",a+b);
+        System.out.println("Summation is =",a+b);
     }
 }

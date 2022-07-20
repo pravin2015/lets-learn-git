cc940a73349d922c18b748a6585a00e55dd4ecfe
Pravinkumar Chavan <pravin.chavan999@gmail.com>
2022-07-20 22:24:08 +0530

    added multiple and modified sum

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 class sum{
-    public static void main(string[] args){
+    public static void main(String[] args){
         int a=5;
         int b=10;
-        System.out.println("Summation is =",a+b);
+        System.out.println("Summation is ="+a+b);
     }
 }
